f76118a6212be1cc42c054bc09640d106547e746
Lucas <lucas.vauclin@ynov.com>
2026-04-09 10:44:49 +0200

    add systeme Case

---
+++ Case.java
@@ -0,0 +1,31 @@
+public class Case {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.piece = null;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
+    public boolean estVide() {
+        return piece == null;
+    }
+}
